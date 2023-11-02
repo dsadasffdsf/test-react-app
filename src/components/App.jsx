1d746cd4e9f8370854fa55b9a9e1ef104e110f24
dsadasffdsf <qwertfdsazxcvg122@mail.ru>
2023-11-02 20:53:30 +0300

    Тестовый комит

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import  Header  from "./Header";
+import  Categories  from "./Categories";
+
+function App() {
+  return (
+    <div className="container m-auto">
+      <Header/>
+      <Categories/>
+    </div>
+  );
+}
+
+export default App;
